fad7a1e7acb53e90452ae8c99fe249dc6b742c58
dashrathkumawat <dashrath.k@shivamitconsultancy.com>
2017-02-15 16:06:49 +0530

    asdf
    asdf

--- app/controllers/aboutmeController.js
+++ app/controllers/aboutmeController.js
@@ -165,13 +165,13 @@ app.controller('aboutmeController', ['$scope', 'localStorageService', 'authServi
         };
         writer.write(_InsertDatasql);
 
-        cordova.plugins.email.open({
-            to: ["gautam.p@shivamitconsultancy.com"], // email addresses for TO field
-            attachments: ["Backup.txt"], // file paths or base64 data streams
-            subject: "test Backup Email", // subject of the email
-            body: "This is just test", // email body (for HTML, set isHtml to true)
-            isHtml: false, // indicats if the body is HTML or plain text
-        }, alert("mail Sent"), null);
+        //cordova.plugins.email.open({
+        //    to: ["gautam.p@shivamitconsultancy.com"], // email addresses for TO field
+        //    attachments: ["Backup.txt"], // file paths or base64 data streams
+        //    subject: "test Backup Email", // subject of the email
+        //    body: "This is just test", // email body (for HTML, set isHtml to true)
+        //    isHtml: false, // indicats if the body is HTML or plain text
+        //}, alert("mail Sent"), null);
     }
 
     var successFnEx = function (sql, count) {
